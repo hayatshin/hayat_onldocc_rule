8f0e39f527aa1be9e6ed18bf4351996bfb4c7b7c
hayatshin <hayat_shin@naver.com>
2025-01-10 14:55:43 +0900

    다듬기

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,132 @@
 import 'package:flutter/material.dart';
 import 'package:hayat_onldocc_rule/strings/rule_string.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  runApp(
+    const MyApp(),
+  );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // String env = "";
-
-  @override
-  void initState() {
-    super.initState();
-    // _initializeEnv();
-  }
-
-  void _initializeEnv() async {
-    // await dotenv.load(fileName: "env");
-    // setState(() {
-    //   env = dotenv.env["CHECK"]!;
-    // });
-  }
-
-  // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      color: Colors.white,
       title: '인지케어 약관',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFF2D78)),
+        scaffoldBackgroundColor: Colors.white,
+        tabBarTheme: const TabBarTheme(
+          dividerHeight: 1,
+          indicator: UnderlineTabIndicator(
+            borderSide: BorderSide(
+              width: 0.5,
+            ),
+          ),
+          overlayColor: WidgetStatePropertyAll(Colors.white),
+          indicatorColor: Color(0xff404040),
+          dividerColor: Color(0xff404040),
+          indicatorSize: TabBarIndicatorSize.tab,
+          unselectedLabelStyle: TextStyle(
+            color: Color(0xff404040),
+            fontWeight: FontWeight.w400,
+            fontSize: 13,
+            height: 18 / 13,
+          ),
+          labelStyle: TextStyle(
+            color: Color(0xff404040),
+            fontWeight: FontWeight.w700,
+            fontSize: 13,
+            height: 18 / 13,
+          ),
+        ),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFFf62459),
+        ),
         useMaterial3: true,
+        fontFamily: "Pretendard",
       ),
       home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({
     super.key,
   });
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
   Widget build(BuildContext context) {
+    const TextStyle defaultStyle = TextStyle(
+      fontWeight: FontWeight.w600,
+      fontSize: 13,
+      height: 18 / 13,
+    );
     return DefaultTabController(
       length: 2,
       child: Scaffold(
         appBar: AppBar(
-          bottom: const TabBar(tabs: [
-            Tab(
-              text: "서비스 이용약관",
-            ),
-            Tab(
-              text: "개인정보처리방침",
-            ),
-          ]),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: TabBarView(
-            physics: const NeverScrollableScrollPhysics(),
-            children: [
-              SingleChildScrollView(child: Text(RuleString().appRule)),
-              SingleChildScrollView(child: Text(RuleString().personalInfo)),
+          backgroundColor: Colors.white,
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                text: "개인정보 처리방침",
+              ),
+              Tab(
+                text: "서비스 이용약관",
+              ),
             ],
           ),
+        ),
+        body: TabBarView(
+          physics: const NeverScrollableScrollPhysics(),
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      RuleString().personalInfo,
+                      style: defaultStyle,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      RuleString().appRule,
+                      style: defaultStyle,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
